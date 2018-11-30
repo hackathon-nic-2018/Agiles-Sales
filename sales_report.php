f7dbc59e4e728e3b01d4189c03f7fa8f2e814d71
morkabarrow <35969816+morkabarrow@users.noreply.github.com>
2018-11-29 22:55:42 -0600

    Update sales_report.php
    los nibeles  requeridos de los usuarios

--- sales_report.php
+++ sales_report.php
@@ -3,3 +3,6 @@ $page_title = 'Reporte de ventas';
 require_once('includes/load.php');
 // Checkin ¿Qué nivel de usuario tiene permiso para ver esta página
 page_require_level(3);
+?>
+<?php
+$page_carding = 'caja de ventas ';

1357ba66b7dcade167dc6651af27b7bef3445a58
morkabarrow <35969816+morkabarrow@users.noreply.github.com>
2018-11-29 22:42:07 -0600

    Update users.php
    de usuarios

--- users.php
+++ users.php
@@ -1,11 +1,27 @@
 <?php
-$page_title = 'Lista de usuarios';
-require_once('includes/load.php');
+  $page_title = 'Lista de usuarios';
+  require_once('includes/load.php');
 ?>
-//nivel del usuario ala de crear diferentes niveles pagina
 <?php
-page_requiere_level(1);
-
-// los que estan alo jado requiere enlos login
-$all_users = find_all_user();
+// Checkin What level user has permission to view this page
+ page_require_level(1);
+//pull out all user form database
+ $all_users = find_all_user();
 ?>
+<?php include_once('layouts/header.php'); ?>
+<div class="row">
+   <div class="col-md-12">
+     <?php echo display_msg($msg); ?>
+   </div>
+</div>
+<div class="row">
+  <div class="col-md-12">
+    <div class="panel panel-default">
+      <div class="panel-heading clearfix">
+        <strong>
+          <span class="glyphicon glyphicon-th"></span>
+          <span>Usuarios</span>
+       </strong>
+         <a href="add_user.php" class="btn btn-info pull-right">Agregar usuario</a>
+      </div>
+     <div class="panel-body">

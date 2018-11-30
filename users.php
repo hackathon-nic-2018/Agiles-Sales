d0c3eb8d12f21574c43404b4933610ce5510535e
morkabarrow <35969816+morkabarrow@users.noreply.github.com>
2018-11-29 22:45:54 -0600

    Update users.php
    de uusarios lo que falta

--- users.php
+++ users.php
@@ -3,9 +3,9 @@
   require_once('includes/load.php');
 ?>
 <?php
-// Checkin What level user has permission to view this page
+// Registro en qué nivel el usuario tiene permiso para ver esta página
  page_require_level(1);
-//pull out all user form database
+//sacar toda la base de datos del formulario de usuario
  $all_users = find_all_user();
 ?>
 <?php include_once('layouts/header.php'); ?>
@@ -25,3 +25,50 @@
          <a href="add_user.php" class="btn btn-info pull-right">Agregar usuario</a>
       </div>
      <div class="panel-body">
+
+       <table class="table table-bordered table-striped">
+        <thead>
+          <tr>
+            <th class="text-center" style="width: 50px;">#</th>
+            <th>Nombre </th>
+            <th>Usuario</th>
+            <th class="text-center" style="width: 15%;">Rol de usuario</th>
+            <th class="text-center" style="width: 10%;">Estado</th>
+            <th style="width: 20%;">Último login</th>
+            <th class="text-center" style="width: 100px;">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+        <?php foreach($all_users as $a_user): ?>
+          <tr>
+           <td class="text-center"><?php echo count_id();?></td>
+           <td><?php echo remove_junk(ucwords($a_user['name']))?></td>
+           <td><?php echo remove_junk(ucwords($a_user['username']))?></td>
+           <td class="text-center"><?php echo remove_junk(ucwords($a_user['group_name']))?></td>
+           <td class="text-center">
+           <?php if($a_user['status'] === '1'): ?>
+            <span class="label label-success"><?php echo "Activo"; ?></span>
+          <?php else: ?>
+            <span class="label label-danger"><?php echo "Inactivo"; ?></span>
+          <?php endif;?>
+           </td>
+           <td><?php echo read_date($a_user['last_login'])?></td>
+           <td class="text-center">
+             <div class="btn-group">
+                <a href="edit_user.php?id=<?php echo (int)$a_user['id'];?>" class="btn btn-xs btn-warning" data-toggle="tooltip" title="Editar">
+                  <i class="glyphicon glyphicon-pencil"></i>
+               </a>
+                <a href="delete_user.php?id=<?php echo (int)$a_user['id'];?>" class="btn btn-xs btn-danger" data-toggle="tooltip" title="Eliminar">
+                  <i class="glyphicon glyphicon-remove"></i>
+                </a>
+                </div>
+           </td>
+          </tr>
+        <?php endforeach;?>
+       </tbody>
+     </table>
+     </div>
+    </div>
+  </div>
+</div>
+  <?php include_once('layouts/footer.php'); ?>

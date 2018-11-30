bc8c1e955a30dc49812ec5f31811b5afa750d070
morkabarrow <35969816+morkabarrow@users.noreply.github.com>
2018-11-30 00:15:37 -0600

    Update product.php
    continuacion del codigo producto

--- product.php
+++ product.php
@@ -9,3 +9,17 @@
        <h1>Bienvenido</h1>
        <p>Iniciar sesión </p>
      </div>
+       <?php echo display_msg($msg); ?>
+      <form method="post" action="auth.php" class="clearfix">
+        <div class="form-group">
+              <label for="username" class="control-label">Usario</label>
+              <input type="name" class="form-control" name="username" placeholder="Usario">
+        </div>
+        <div class="form-group">
+            <label for="Password" class="control-label">Contraseña</label>
+            <input type="password" name= "password" class="form-control" placeholder="Contraseña">
+        </div>
+        <div class="form-group">
+                <button type="submit" class="btn btn-info  pull-right">Entrar</button>
+        </div>
+    </form>

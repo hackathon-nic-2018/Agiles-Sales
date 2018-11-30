3461914bf3660381e34c2d7273c0a9cb2aa10d3a
morkabarrow <35969816+morkabarrow@users.noreply.github.com>
2018-11-30 02:00:47 -0600

    Create categorie.php
    categorias de productos y todo

---
+++ categorie.php
@@ -0,0 +1,63 @@
+<?php
+  $page_title = 'Lista de categorías';
+  require_once('includes/load.php');
+  // Checkin What level user has permission to view this page
+  page_require_level(1);
+  
+  $all_categories = find_all('categories')
+?>
+<?php
+ if(isset($_POST['add_cat'])){
+   $req_field = array('categorie-name');
+   validate_fields($req_field);
+   $cat_name = remove_junk($db->escape($_POST['categorie-name']));
+   if(empty($errors)){
+      $sql  = "INSERT INTO categories (name)";
+      $sql .= " VALUES ('{$cat_name}')";
+      if($db->query($sql)){
+        $session->msg("s", "Categoría agregada exitosamente.");
+        redirect('categorie.php',false);
+      } else {
+        $session->msg("d", "Lo siento, registro falló");
+        redirect('categorie.php',false);
+      }
+   } else {
+     $session->msg("d", $errors);
+     redirect('categorie.php',false);
+   }
+ }
+?>
+<?php include_once('layouts/header.php'); ?>
+
+  <div class="row">
+     <div class="col-md-12">
+       <?php echo display_msg($msg); ?>
+     </div>
+  </div>
+   <div class="row">
+    <div class="col-md-5">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <strong>
+            <span class="glyphicon glyphicon-th"></span>
+            <span>Agregar categoría</span>
+         </strong>
+        </div>
+        <div class="panel-body">
+          <form method="post" action="categorie.php">
+            <div class="form-group">
+                <input type="text" class="form-control" name="categorie-name" placeholder="Nombre de la categoría" required>
+            </div>
+            <button type="submit" name="add_cat" class="btn btn-primary">Agregar categoría</button>
+        </form>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-7">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <strong>
+          <span class="glyphicon glyphicon-th"></span>
+          <span>Lista de categorías</span>
+       </strong>
+      </div>

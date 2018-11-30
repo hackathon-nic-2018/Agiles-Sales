663acb1fc1f74bccc04dc32d4c67e2be6d37f7a1
morkabarrow <35969816+morkabarrow@users.noreply.github.com>
2018-11-30 00:53:55 -0600

    Create edit_user.php
    para editar los usuarios que tenemos

---
+++ edit_user.php
@@ -0,0 +1,83 @@
+<?php
+  $page_title = 'Editar Usuario';
+  require_once('includes/load.php');
+  // Checkin What level user has permission to view this page
+   page_require_level(1);
+?>
+<?php
+  $e_user = find_by_id('users',(int)$_GET['id']);
+  $groups  = find_all('user_groups');
+  if(!$e_user){
+    $session->msg("d","Missing user id.");
+    redirect('users.php');
+  }
+?>
+
+<?php
+//Update User basic info
+  if(isset($_POST['update'])) {
+    $req_fields = array('name','username','level');
+    validate_fields($req_fields);
+    if(empty($errors)){
+             $id = (int)$e_user['id'];
+           $name = remove_junk($db->escape($_POST['name']));
+       $username = remove_junk($db->escape($_POST['username']));
+          $level = (int)$db->escape($_POST['level']);
+       $status   = remove_junk($db->escape($_POST['status']));
+            $sql = "UPDATE users SET name ='{$name}', username ='{$username}',user_level='{$level}',status='{$status}' WHERE id='{$db->escape($id)}'";
+         $result = $db->query($sql);
+          if($result && $db->affected_rows() === 1){
+            $session->msg('s',"Acount Updated ");
+            redirect('edit_user.php?id='.(int)$e_user['id'], false);
+          } else {
+            $session->msg('d',' Lo siento no se actualizó los datos.');
+            redirect('edit_user.php?id='.(int)$e_user['id'], false);
+          }
+    } else {
+      $session->msg("d", $errors);
+      redirect('edit_user.php?id='.(int)$e_user['id'],false);
+    }
+  }
+?>
+<?php
+// Update user password
+if(isset($_POST['update-pass'])) {
+  $req_fields = array('password');
+  validate_fields($req_fields);
+  if(empty($errors)){
+           $id = (int)$e_user['id'];
+     $password = remove_junk($db->escape($_POST['password']));
+     $h_pass   = sha1($password);
+          $sql = "UPDATE users SET password='{$h_pass}' WHERE id='{$db->escape($id)}'";
+       $result = $db->query($sql);
+        if($result && $db->affected_rows() === 1){
+          $session->msg('s',"Se ha actualizado la contraseña del usuario. ");
+          redirect('edit_user.php?id='.(int)$e_user['id'], false);
+        } else {
+          $session->msg('d','No se pudo actualizar la contraseña de usuario..');
+          redirect('edit_user.php?id='.(int)$e_user['id'], false);
+        }
+  } else {
+    $session->msg("d", $errors);
+    redirect('edit_user.php?id='.(int)$e_user['id'],false);
+  }
+}
+
+?>
+<?php include_once('layouts/header.php'); ?>
+ <div class="row">
+   <div class="col-md-12"> <?php echo display_msg($msg); ?> </div>
+  <div class="col-md-6">
+     <div class="panel panel-default">
+       <div class="panel-heading">
+        <strong>
+          <span class="glyphicon glyphicon-th"></span>
+          Actualiza cuenta <?php echo remove_junk(ucwords($e_user['name'])); ?>
+        </strong>
+       </div>
+       <div class="panel-body">
+          <form method="post" action="edit_user.php?id=<?php echo (int)$e_user['id'];?>" class="clearfix">
+            <div class="form-group">
+                  <label for="name" class="control-label">Nombres</label>
+                  <input type="name" class="form-control" name="name" value="<?php echo remove_junk(ucwords($e_user['name'])); ?>">
+            </div>

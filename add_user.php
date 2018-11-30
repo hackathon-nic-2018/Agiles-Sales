743443b28b0d11d044f7ecb66274f931607478be
morkabarrow <35969816+morkabarrow@users.noreply.github.com>
2018-11-30 02:17:14 -0600

    Create add_user.php
    agregar usuarios

---
+++ add_user.php
@@ -0,0 +1,49 @@
+<?php
+  $page_title = 'Agregar usuarios';
+  require_once('includes/load.php');
+  // Checkin What level user has permission to view this page
+  page_require_level(1);
+  $groups = find_all('user_groups');
+?>
+<?php
+  if(isset($_POST['add_user'])){
+
+   $req_fields = array('full-name','username','password','level' );
+   validate_fields($req_fields);
+
+   if(empty($errors)){
+           $name   = remove_junk($db->escape($_POST['full-name']));
+       $username   = remove_junk($db->escape($_POST['username']));
+       $password   = remove_junk($db->escape($_POST['password']));
+       $user_level = (int)$db->escape($_POST['level']);
+       $password = sha1($password);
+        $query = "INSERT INTO users (";
+        $query .="name,username,password,user_level,status";
+        $query .=") VALUES (";
+        $query .=" '{$name}', '{$username}', '{$password}', '{$user_level}','1'";
+        $query .=")";
+        if($db->query($query)){
+          //sucess
+          $session->msg('s'," Cuenta de usuario ha sido creada");
+          redirect('add_user.php', false);
+        } else {
+          //failed
+          $session->msg('d',' No se pudo crear la cuenta.');
+          redirect('add_user.php', false);
+        }
+   } else {
+     $session->msg("d", $errors);
+      redirect('add_user.php',false);
+   }
+ }
+?>
+<?php include_once('layouts/header.php'); ?>
+  <?php echo display_msg($msg); ?>
+  <div class="row">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <strong>
+          <span class="glyphicon glyphicon-th"></span>
+          <span>Agregar usuario</span>
+       </strong>
+      </div>

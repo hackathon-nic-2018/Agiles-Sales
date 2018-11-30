88acc3a0d83e877ab904e15b4bdd043f6a9b4774
morkabarrow <35969816+morkabarrow@users.noreply.github.com>
2018-11-30 00:14:38 -0600

    Create product.php
    los indices delos productos

---
+++ product.php
@@ -0,0 +1,11 @@
+<?php
+  ob_start();
+  require_once('includes/load.php');
+  if($session->isUserLoggedIn(true)) { redirect('home.php', false);}
+?>
+<?php include_once('layouts/header.php'); ?>
+<div class="login-page">
+    <div class="text-center">
+       <h1>Bienvenido</h1>
+       <p>Iniciar sesión </p>
+     </div>

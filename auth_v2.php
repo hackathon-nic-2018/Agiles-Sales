1f957ba0aaa4643fbd562cc0a7cab47cf0273344
morkabarrow <35969816+morkabarrow@users.noreply.github.com>
2018-11-30 02:04:27 -0600

    Update auth_v2.php
    primera parte

---
+++ auth_v2.php
@@ -0,0 +1,12 @@
+<?php include_once('includes/load.php'); ?>
+<?php
+$req_fields = array('username','password' );
+validate_fields($req_fields);
+$username = remove_junk($_POST['username']);
+$password = remove_junk($_POST['password']);
+
+  if(empty($errors)){
+
+    $user = authenticate_v2($username, $password);
+
+      

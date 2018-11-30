627887f114b6785111bed852f84c78bb98b83a4d
morkabarrow <35969816+morkabarrow@users.noreply.github.com>
2018-11-30 01:23:27 -0600

    Create edit_product.php
    edicion de producto pana

---
+++ edit_product.php
@@ -0,0 +1,50 @@
+<?php
+  $page_title = 'Editar producto';
+  require_once('includes/load.php');
+  // Checkin ¿Qué nivel de usuario tiene permiso para ver esta página?
+   page_require_level(2);
+?>
+<?php
+$product = find_by_id('products',(int)$_GET['id']);
+$all_categories = find_all('categories');
+$all_photo = find_all('media');
+if(!$product){
+  $session->msg("d","Missing product id.");
+  redirect('product.php');
+}
+?>
+<?php
+ if(isset($_POST['product'])){
+    $req_fields = array('product-title','product-categorie','product-quantity','buying-price', 'saleing-price' );
+    validate_fields($req_fields);
+
+   if(empty($errors)){
+       $p_name  = remove_junk($db->escape($_POST['product-title']));
+       $p_cat   = (int)$_POST['product-categorie'];
+       $p_qty   = remove_junk($db->escape($_POST['product-quantity']));
+       $p_buy   = remove_junk($db->escape($_POST['buying-price']));
+       $p_sale  = remove_junk($db->escape($_POST['saleing-price']));
+       if (is_null($_POST['product-photo']) || $_POST['product-photo'] === "") {
+         $media_id = '0';
+       } else {
+         $media_id = remove_junk($db->escape($_POST['product-photo']));
+       }
+       $query   = "UPDATE products SET";
+       $query  .=" name ='{$p_name}', quantity ='{$p_qty}',";
+       $query  .=" buy_price ='{$p_buy}', sale_price ='{$p_sale}', categorie_id ='{$p_cat}',media_id='{$media_id}'";
+       $query  .=" WHERE id ='{$product['id']}'";
+       $result = $db->query($query);
+               if($result && $db->affected_rows() === 1){
+                 $session->msg('s',"Producto ha sido actualizado. ");
+                 redirect('product.php', false);
+               } else {
+                 $session->msg('d',' Lo siento, actualización falló.');
+                 redirect('edit_product.php?id='.$product['id'], false);
+               }
+
+   } else{
+       $session->msg("d", $errors);
+       redirect('edit_product.php?id='.$product['id'], false);
+   }
+
+ }

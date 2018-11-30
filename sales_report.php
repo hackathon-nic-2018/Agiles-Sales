83d75dffd96c94f109cf5f274b00c6c21c6e0ab9
morkabarrow <35969816+morkabarrow@users.noreply.github.com>
2018-11-29 23:00:30 -0600

    Update sales_report.php
    correciion delo codigos de usuario

--- sales_report.php
+++ sales_report.php
@@ -1,8 +1,12 @@
 <?php
 $page_title = 'Reporte de ventas';
-require_once('includes/load.php');
-// Checkin ¿Qué nivel de usuario tiene permiso para ver esta página
-page_require_level(3);
+  require_once('includes/load.php');
+  // Checkin What level user has permission to view this page
+   page_require_level(3);
 ?>
-<?php
-$page_carding = 'caja de ventas ';
+<?php include_once('layouts/header.php'); ?>
+<div class="row">
+  <div class="col-md-6">
+    <?php echo display_msg($msg); ?>
+  </div>
+</div>
